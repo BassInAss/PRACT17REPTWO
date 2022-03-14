879afd5c74c5d9f4ea02b4354ec863b29610fd28
Viktor <viktor.kononenko.2003@mail.ru>
2022-03-14 16:17:50 +0500

    Добавлен тест заголовка

---
+++ PRACT17KON/HeaderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chromium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PRACT17KON
+{
+    [TestFixture]
+    public class HeaderTest
+    {
+        [TestCase]
+        public void MainTitle()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Url = "https://esia.gosuslugi.ru/login/";
+            Assert.AreEqual("Портал государственных услуг Российской Федерации", webDriver.Title);
+            ///html/body/esia-root/div/esia-idp/div/div[1]/form/div[4]/button
+            webDriver.Close();
+        }
+    }
+}
