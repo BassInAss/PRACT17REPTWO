7dedab4cf13401dd0681172e30f1f4be4f0420bf
Viktor <viktor.kononenko.2003@mail.ru>
2022-03-14 17:01:51 +0500

    Добавлен тест появления надписи

--- PRACT17KON/HeaderTest.cs
+++ PRACT17KON/HeaderTest.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using OpenQA.Selenium.Chromium;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace PRACT17KON
 {
@@ -25,9 +26,47 @@ namespace PRACT17KON
         public void NameButton()
         {
             webDriver.Url = "https://esia.gosuslugi.ru/login/";
-            IWebElement input = webDriver.FindElement(By.XPath("html/body/esia-root/div/esia-idp/div/div[1]/form/div[4]/button"));
-            Assert.AreEqual("Войти", input.Text);
+            if (IsLoaded("html/body/esia-root/div/esia-idp/div/div[1]/form/div[4]/button"))
+            {
+                IWebElement input = webDriver.FindElement(By.XPath("html/body/esia-root/div/esia-idp/div/div[1]/form/div[4]/button"));
+                Assert.AreEqual("Войти", input.Text);
+            }
             webDriver.Close();
+        }
+        [TestCase]
+        public void IsLogAndPass()
+        {
+            webDriver.Url = "https://esia.gosuslugi.ru/login/";
+                IWebElement button = webDriver.FindElement(By.XPath("html/body/esia-root/div/esia-idp/div/div[1]/form/div[4]/button"));
+                button.Click();
+                IWebElement str = webDriver.FindElement(By.XPath("/html/body/esia-root/div/esia-idp/div/div[1]/form/div[2]/esia-input-password/div/div"));
+                Assert.IsTrue(str.Displayed == true);
+            webDriver.Close();
+        }
+        public bool IsLoaded(string XPath)
+        {
+            var wait = new WebDriverWait(webDriver, new TimeSpan(0, 0, 30));
+            //установка условия окончания ожидания
+            var element = wait.Until(condition =>
+            {
+                try
+                {
+                    //до тех пор, пока указанный элемент не станет видим (Displayed == true)
+                    var elementToBeDisplayed =
+                        webDriver.FindElement(By.XPath("/html/body/table/tbody/tr[2]/td/form/div[2]/button"));
+                    return elementToBeDisplayed.Displayed;
+                }
+                //в случае, если такого элемента нет
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+            return element;
         }
     }
 }
